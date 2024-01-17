5d0f37b1df2f57d73d6c2803037a8e2271bbfc3d
Alena Korenkovich <adkorenkovich@avito.ru>
2024-01-17 22:58:42 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const menuButtons = document.querySelectorAll('.dropdown_head button');
+
+for (const menuButton of menuButtons) {
+  menuButton.addEventListener('click', (event) => {
+    event.stopPropagation();
+
+    const submenu = menuButton.parentElement.nextElementSibling;
+    if (menuButton.classList.contains('dropdown_closed')) {
+      menuButton.classList.remove('dropdown_closed');
+      submenu.classList.remove('hidden');
+    } else {
+      menuButton.classList.add('dropdown_closed');
+      submenu.classList.add('hidden');
+    }
+  });
+}
+
+/* ------------------------------------------------------------------------------------------- */
+
+const sections = document.querySelectorAll("section[id]");
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  let scrollY = window.scrollY;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+
+    const sectionTop = (current.getBoundingClientRect().top + window.scrollY) - 50;
+    const sectionId = current.getAttribute("id");
+
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector("nav a[href*=" + sectionId + "]").parentElement.classList.add("selected_menu_option");
+    } else {
+      document.querySelector("nav a[href*=" + sectionId + "]").parentElement.classList.remove("selected_menu_option");
+    }
+  });
+}
